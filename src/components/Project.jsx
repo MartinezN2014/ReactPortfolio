45de4429ed070706e6fd91f07b3ab723d588c252
MartinezN2014 <2014nmartinez@gmail.com>
2023-10-26 00:49:06 -0400

    This should add my images of my projects with their respective links to github

---
+++ src/components/Project.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+// import Project from "../pages/Portfolio"
+
+function Project({ title, image, deployedLink, githubLink }) {
+  return (
+    <div className="project">
+      <h3>{title}</h3>
+      <img className="projectImageClass" src={image} alt={title} />
+      <div className="projectLinks">
+      <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+        View Project       
+      </a>
+      <a href={githubLink} target="_blank" rel="noopener noreferrer">
+        GitHub Repository
+      </a>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
