bf1454a3117603e49ff0e526d4c2909175e30d9a
MartinezN2014 <2014nmartinez@gmail.com>
2023-10-26 00:45:52 -0400

    header that has all my paths

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+function Header() {
+  const location = useLocation();
+
+  return (
+    <header>
+      <div className="header-content">
+        <h1>Nelson Martinez</h1>
+      </div>
+      <nav>
+        <ul>
+          <li className="navButtons">
+            <Link to="/about-me" className={location.pathname === '/about-me' ? 'active' : ''}>
+              About Me
+            </Link>
+          </li>
+          <li className="navButtons">
+            <Link to="/portfolio" className={location.pathname === '/portfolio' ? 'active' : ''}>
+              Portfolio
+            </Link>
+          </li>
+          <li className="navButtons">
+            <Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>
+              Contact Me
+            </Link>
+          </li>
+          <li className="navButtons">
+            <Link to="/resume" className={location.pathname === '/resume' ? 'active' : ''}>
+              Resume
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
