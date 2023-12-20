9d8578818b26c6da4b5054c035c0c4f386948329
MartinezN2014 <2014nmartinez@gmail.com>
2023-12-20 09:53:35 -0500

    Changed the aboutme page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -7,18 +7,18 @@ const AboutMe = () => {
       <img className='profileImage' src="/pfp.jpg" alt="My Photo" />
       <section>
         <h2>Experience</h2>
-        <ul>
-          <li>Graduated from my Full-Stack Web Development program at UPENN</li>
-          <li>Did around 6 years of warehouse work and even learned to drive forklifts</li>
-        </ul>
+
+          <p>I recently graduated from UPENN bootcamp program where I worked on multiple projects and homework assignments in a agile environment.</p>
+          <p>During my time there I managed to pick up on multiple skills such as HTML, CSS, and JavaScript.</p>
+          <p>I also worked with MySql and MongoDB for backend work and even worked with React applications.</p>
+       
       </section>
       <section>
         <h2>Fun Facts</h2>
-        <ul>
-          <li>Esports Competitor</li>
-          <li>Built my own PC</li>
-          <li>Love to play video games</li>
-        </ul>
+        <p>I am always looking foward to learning new skills and techniques.
+          I also love working with others so that I can learn from others as well.
+        </p>
+        <p>I also am big on video games and even compete in E-sports competitions.</p>
       </section>
     </section>
   );
